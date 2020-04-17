344161da22ebb679987d5da8a51ac653c3ea182c
Merijn <99054535@mydavinci.nl>
2020-04-17 14:05:44 +0200

    css toegevoegd

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
-require "./function.php"
+require "./function.php";
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/B3W7O1/style.css">
+    <script src="https://kit.fontawesome.com/ca88a28f8c.js" crossorigin="anonymous"></script>
+    <title>Deze opdracht</title>
 </head>
 <body>
+    <h1>Planning</h1>
+    
+    
 </body>
 </html>
