223a75206b0190263f404efb0e07a3af9f4ca634
Merijn <99054535@mydavinci.nl>
2020-04-22 12:21:02 +0200

    een klein beetje vooruit gegaan

---
+++ create.php
@@ -0,0 +1,19 @@
+<?php
+require "./function.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/B3W7O1/style.css">
+    <title>Create planning</title>
+</head>
+<body>
+    <h1><a id='headerCreate' href='planning.php'>Return to planning?</a></h1>
+    <label for='name'>Name of game:</label><input type='text' name='name' id='name'>
+    <label for='startdate'>Start date:</label><input type='date' name='startdate' id='startdate'>
+    <label for='starttime'>Start time:</label><input type='time' name='starttime' id='starttime'>
+</body>
+</html>
--- planning.php
+++ planning.php
@@ -15,8 +15,7 @@ $result = getGames();
 <body>
     <h1 id='h1Home'><a id='headerHome' href="index.php">Home</a></h1> 
     <h1><a id='headerPlanning' href="planning.php">planning</a></h1>
-    <label for='startdate'></label><input type='date' name='startdate' id='startdate'>
-    <label for='starttime'></label><input type='time' name='starttime' id='starttime'>
+    <h1><a id='headerPlanning' href="create.php">Create planning</a></h1>
 <?php
    foreach($result as $planning){
 
